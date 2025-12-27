3eb7579c2127bfd1480a1f1babafc373c1c9eaf5
Joy1526 <ahjoy1526@gmail.com>
2025-12-28 00:56:10 +0600

    HomePage 95% Done

--- Local-Volunteer-Radar/Frontend/src/App.jsx
+++ Local-Volunteer-Radar/Frontend/src/App.jsx
@@ -1,8 +1,14 @@
-
+import HomePage from "./components/HomePage.jsx";
+import './App.css'
 
 
 function App() {
-
+    return (
+        <div className="App">
+            <HomePage/>
+        </div>
+    )
 }
 
 export default App
+
