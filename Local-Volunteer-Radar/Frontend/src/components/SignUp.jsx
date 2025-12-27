a75557739614c7f38d3bd441fba012204dc15a22
shihabcs <shihabahmedfahim150@gmail.com>
2025-12-27 22:00:19 +0600

    Login Complete

---
+++ Local-Volunteer-Radar/Frontend/src/components/SignUp.jsx
@@ -0,0 +1,10 @@
+const SignUp = () => {
+    return (
+        <div style={{ padding: '20px' }}>
+            <h1>Sign Up Page</h1>
+            <p>Sign up page design coming soon...</p>
+        </div>
+    );
+};
+
+export default SignUp;
